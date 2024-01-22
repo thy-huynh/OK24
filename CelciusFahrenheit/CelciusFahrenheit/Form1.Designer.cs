f2e46447393cd69521232a28dd0bc4c3a9f50449
thy-huynh <thy-huynh@hotmail.com>
2024-01-22 13:07:31 +0200

    CelciusFahrenheit

---
+++ CelciusFahrenheit/CelciusFahrenheit/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CelciusFahrenheit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CelciusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.AsteetTB = new System.Windows.Forms.TextBox();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna asteet:";
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.Location = new System.Drawing.Point(458, 54);
+            this.MuunnaBT.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(119, 38);
+            this.MuunnaBT.TabIndex = 1;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = true;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FahrenheitRB);
+            this.groupBox1.Controls.Add(this.CelciusRB);
+            this.groupBox1.Location = new System.Drawing.Point(254, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.groupBox1.Size = new System.Drawing.Size(183, 130);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Miten muunnan?";
+            // 
+            // CelciusRB
+            // 
+            this.CelciusRB.AutoSize = true;
+            this.CelciusRB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CelciusRB.Location = new System.Drawing.Point(9, 42);
+            this.CelciusRB.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.CelciusRB.Name = "CelciusRB";
+            this.CelciusRB.Size = new System.Drawing.Size(91, 29);
+            this.CelciusRB.TabIndex = 0;
+            this.CelciusRB.TabStop = true;
+            this.CelciusRB.Text = "Celcius";
+            this.CelciusRB.UseVisualStyleBackColor = true;
+            // 
+            // FahrenheitRB
+            // 
+            this.FahrenheitRB.AutoSize = true;
+            this.FahrenheitRB.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FahrenheitRB.Location = new System.Drawing.Point(9, 81);
+            this.FahrenheitRB.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.FahrenheitRB.Name = "FahrenheitRB";
+            this.FahrenheitRB.Size = new System.Drawing.Size(123, 29);
+            this.FahrenheitRB.TabIndex = 1;
+            this.FahrenheitRB.TabStop = true;
+            this.FahrenheitRB.Text = "Fahrenheit";
+            this.FahrenheitRB.UseVisualStyleBackColor = true;
+            // 
+            // AsteetTB
+            // 
+            this.AsteetTB.Location = new System.Drawing.Point(140, 54);
+            this.AsteetTB.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.AsteetTB.Name = "AsteetTB";
+            this.AsteetTB.Size = new System.Drawing.Size(104, 33);
+            this.AsteetTB.TabIndex = 3;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(37, 169);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(76, 25);
+            this.VastausLB.TabIndex = 4;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 224);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.AsteetTB);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Aste muunnin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button MuunnaBT;
+        private GroupBox groupBox1;
+        private RadioButton FahrenheitRB;
+        private RadioButton CelciusRB;
+        private TextBox AsteetTB;
+        private Label VastausLB;
+    }
+}
